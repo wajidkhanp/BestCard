09481d94a4ba0d0b4d3cd889a5e3daa77a04dd6d
wajid <wajid@live.ca>
2020-06-14 04:58:46 -0700

    Initial Commit

---
+++ BestCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BestCard
+//
+//  Created by Wajid K Patan on 6/14/20.
+//  Copyright © 2020 wajidInc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
